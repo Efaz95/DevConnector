21b495d9738cf48dc4562cb89a2cfdfad728885c
Efaz Khan <efaz@MacBook-Pro.local>
2020-06-01 13:50:21 -0400

    test routes created

--- server.js
+++ server.js
@@ -15,5 +15,6 @@ app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on ${PORT}✅`));
+app.listen(PORT, () => console.log(`Server started on ${PORT} ✅`));
+
 
